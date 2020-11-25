9af1eac6ba5a1fe36696680de264e45bc7c03cc6
Graeme Blair <graemeblair@users.noreply.github.com>
2020-11-24 22:04:49 -0500

    footer

--- R/bs4_book.R
+++ R/bs4_book.R
@@ -399,7 +399,8 @@ tweak_navbar <- function(html, toc, active = "", rmd_index = NULL, repo = NULL) 
     repo_view <- NULL
   }
 
-  template_link(html, ".//a[@id='download-code']", paste0("./", toc$id[toc$file_name == active], ".R"))
+  print(toc$id[toc$file_name == active & toc$level == 1])
+  template_link(html, ".//a[@id='download-code']", paste0("./", toc$id[toc$file_name == active & toc$level == 1], ".R"))
 
   template_link(html, ".//a[@id='book-repo']", repo)
   template_link(html, ".//a[@id='book-source']", repo_view)
